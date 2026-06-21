52179870ecf55396a65040bbf41d49a42bd4a9cf
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2026-06-21 23:47:56 +0300

    Simplify

--- angular-application-deployment-web/src/main.ts
+++ angular-application-deployment-web/src/main.ts
@@ -1,23 +1,25 @@
-import { enableProdMode, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
-import { platformBrowser, BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import {
+  enableProdMode,
+  provideZoneChangeDetection,
+  importProvidersFrom,
+} from "@angular/core";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideHttpClient, withXhr } from "@angular/common/http";
 
-
-import { environment } from './environments/environment';
-import { provideHttpClient, withXhr } from '@angular/common/http';
-import { HelloService } from './app/shared/services/hello.service';
-import { AppRoutingModule } from './app/modules/routing/app-routing.module';
-import { AppComponent } from './app/app.component';
+import { environment } from "./environments/environment";
+import { HelloService } from "./app/shared/services/hello.service";
+import { AppRoutingModule } from "./app/modules/routing/app-routing.module";
+import { AppComponent } from "./app/app.component";
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule),
-        provideHttpClient(withXhr()),
-        provideZoneChangeDetection(),
-        HelloService
-    ]
-})
-  .catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(AppRoutingModule),
+    provideHttpClient(withXhr()),
+    provideZoneChangeDetection(),
+    HelloService,
+  ],
+}).catch((err) => console.error(err));
