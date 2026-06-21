f8876188c01ddfd474f6f63ad5ff79161798283f
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2026-06-21 23:17:59 +0300

    Bootstrap the application using standalone APIs

--- angular-application-deployment-web/src/main.ts
+++ angular-application-deployment-web/src/main.ts
@@ -1,12 +1,23 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowser } from '@angular/platform-browser';
+import { enableProdMode, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { platformBrowser, BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { provideHttpClient, withXhr } from '@angular/common/http';
+import { HelloService } from './app/shared/services/hello.service';
+import { AppRoutingModule } from './app/modules/routing/app-routing.module';
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowser().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule),
+        provideHttpClient(withXhr()),
+        provideZoneChangeDetection(),
+        HelloService
+    ]
+})
   .catch(err => console.error(err));
